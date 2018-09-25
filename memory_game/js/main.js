9ae2bf30c78a6913c44c0fe8cee5d5d17e2fe04a
Jason <jason@meetmycampus.com>
2018-09-24 22:24:08 -0400

    final version

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,20 +26,33 @@ var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+		alert("Sorry, try again.");	
 	}
 }
-var flipCard = function(cardId){
-
+var flipCard = function(e){
+	var cardId = e.getAttribute("data-id");
+	console.log(cardId)
 	cardsInPlay.push(cards[cardId].rank);
-
-	 // checkForMatch();
+	e.setAttribute("src",cards[cardId].cardImage)
+	checkForMatch();
 
 	 console.log("User flipped " + cards[cardId].rank + " image path: " + cards[cardId].cardImage + " suit: " + cards[cardId].suit)
 
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src","images/back.png");
+		cardElement.setAttribute("data-id",i);
+		cardElement.addEventListener("click",function(){
+			flipCard(this);
+		});
+		document.getElementById("game-board").appendChild(cardElement)
+	}
+}
+
+createBoard();
